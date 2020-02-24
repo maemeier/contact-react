12c31c80d746176b372c5b59e313f9f03a2dddcb
Mae Kangsadan <maemeier@live.com>
2020-02-25 00:15:24 +0100

    contact routes

--- models/contact.js
+++ models/contact.js
@@ -1,22 +1,31 @@
 const mongoose = require("mongoose");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
 
 const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  },
   name: {
     type: String,
     required: true
   },
   email: {
     type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
     required: true
   },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: "personal"
+  },
+
   date: {
     type: Date,
     default: Date.now
   }
 });
-module.exports = mongoose.model("user", ContactSchema);
+module.exports = mongoose.model("contact", ContactSchema);
