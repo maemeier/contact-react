36011d999438633b0d29f57ebf60090f95322d5b
Mae Kangsadan <maemeier@live.com>
2020-02-25 23:20:45 +0100

    set contact and global state

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,3 +2,52 @@ import React, { useReducer } from "react";
 import uuid from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from "../type";
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "mae meier",
+        email: "maemeier@live.com",
+        phone: "079-231-3456",
+        type: "professional"
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+  // Add contact
+
+  // delete contacts
+
+  // set current contacts
+
+  // clear current contacts
+
+  //update contacts
+
+  // filter contacts
+
+  // clear filter
+
+  return (
+    <ContactContext.Provider>
+      value=
+      {{
+        contacts: state.contacts
+      }}
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+export default ContactState;
