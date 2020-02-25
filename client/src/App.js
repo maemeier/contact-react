e4da12cdb7b8fafe6734604fd65ca50869d15fc2
Mae Kangsadan <maemeier@live.com>
2020-02-25 22:00:33 +0100

    pages components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Navbar from "./components/layout/Navbar";
 
 import "./App.css";
 
 const App = () => {
-  return <div>This is App component</div>;
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Navbar = ({ title, icon }) => {
-  return <div>This is Navbar</div>;
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/"> Home </Link>
+        </li>
+        <li>
+          <Link to="/about"> About </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 Navbar.propTypes = {
