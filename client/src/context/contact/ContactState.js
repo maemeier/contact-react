14ce4321121a2de0f2499ee160e94447641edf82
Mae Kangsadan <maemeier@live.com>
2020-02-26 23:35:21 +0100

    clear filter

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,7 +38,8 @@ const ContactState = props => {
         type: "professional"
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -71,7 +72,14 @@ const ContactState = props => {
 
   // filter contacts
 
+  const filterContact = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
+
   // clear filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
--- client/src/context/type.js
+++ client/src/context/type.js
@@ -6,3 +6,4 @@ export const UPDATE_CONTACT = "UPDATE_CONTACT";
 export const FILTER_CONTACTS = "FILTER_CONTACTS";
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
+export const CLEAR_FILTER = "CLEAR_FILTER";
