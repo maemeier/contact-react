916c8539081411dafe72eb72b1d7f125febf4bf5
Mae Kangsadan <maemeier@live.com>
2020-03-01 18:31:15 +0100

    set new private

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <Private Route exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
